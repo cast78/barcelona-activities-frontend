f0eb14e0ec6ba70d92a4ffe969e5f1fb24a39a6c
cast78 <camilocastro78@gmail.com>
2026-06-14 12:58:32 +0200

    fix LanguageSwitcher

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -1,4 +1,4 @@
-﻿import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useLanguage } from '../i18n/useT';
 
 const LANGUAGES = [
