e6219083101b70995ed32026bf92a3d7123f9864
cast78 <camilocastro78@gmail.com>
2026-08-03 23:35:14 +0200

    feat: boton modo ciudad rojo/azul con badge-icon.png transparente

--- src/App.tsx
+++ src/App.tsx
@@ -594,9 +594,11 @@ function App() {
                   title={trackingMode ? 'Desactivar modo ciudad' : 'Activar modo ciudad'}
                   aria-label={trackingMode ? 'Desactivar modo ciudad' : 'Activar modo ciudad'}
                 >
-                  <svg viewBox="0 0 24 24" width="16" height="16" fill="white" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
-                  </svg>
+                  <img
+                    src="/badge-icon.png"
+                    alt={trackingMode ? 'Modo ciudad activo' : 'Activar modo ciudad'}
+                    style={{ width: 22, height: 22, objectFit: 'contain' }}
+                  />
                 </button>
 
                 {/* Callout de primera visita */}
